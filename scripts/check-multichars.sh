54005600832ce777f8b5ec1dbfe5eeefd9bb2e3d
Francis Tyers <ftyers@prompsit.com>
2020-08-06 11:36:44 +0100

    add missing files

---
+++ scripts/check-multichars.sh
@@ -0,0 +1,16 @@
+
+head=`grep -nH 'LEXICON Root' apertium-nhi.nhi.lexc  | cut -f2 -d':'`
+
+for tag in `cat apertium-nhi.nhi.lexc | grep -o '%<[^>]\+%>' | sort -u`;  do
+	found=`head -n $head apertium-nhi.nhi.lexc | grep "$tag" | wc -l`
+	if [[ $found -eq 0 ]]; then
+		echo "$tag                         !";
+	fi
+done
+
+for tag in `cat apertium-nhi.nhi.lexc | grep -o '%{[^}]\+%}' | sort -u`;  do
+	found=`head -n $head apertium-nhi.nhi.lexc | grep "$tag" | wc -l`
+	if [[ $found -eq 0 ]]; then
+		echo "$tag                         !";
+	fi
+done
