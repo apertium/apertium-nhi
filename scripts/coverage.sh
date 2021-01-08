4daaa542b9b43237bed741d90ed35d9dffe8e5bb
Robert Pugh <robert.pugh408@gmail.com>
2021-01-07 17:18:22 -0800

    simple honorific vowel harmony

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,7 +5,8 @@
 
 cd ..
 
-PATH_TO_CORPUS=corpus/omitlan_1.txt
+#"corpus/tiquitini_ACK.txt corpus/omitlan_1.txt corpus/omitlan_2.txt"
+PATH_TO_CORPUS=corpus/tiquitini_ACK.txt
 
 NUM_WORDS=$(cat $PATH_TO_CORPUS | wc -w)
 NUM_UNIQUE_WORDS=$(for word in $(cat $PATH_TO_CORPUS); do echo "$word"; done | sort -u | wc -w)
@@ -16,6 +17,7 @@ NUM_UNIQUE_WORDS_PARSED=$(for word in $MATCHED; do echo "$word"; done | sort -u 
 TOKEN_COVERAGE=$(awk -v nparsed="$NUM_WORDS_PARSED" -v ntokens="$NUM_WORDS" 'BEGIN { print nparsed / ntokens ; exit 0}')
 TYPE_COVERAGE=$(awk -v nparsed="$NUM_UNIQUE_WORDS_PARSED" -v ntokens="$NUM_UNIQUE_WORDS" 'BEGIN { print nparsed / ntokens ; exit 0}')
 
-echo "Num words in corpus: $NUM_WORDS"
+echo "Num words in corpus:  $NUM_WORDS"
+echo "Num unique words: $NUM_UNIQUE_WORDS"
 echo "Token coverage:      $TOKEN_COVERAGE"
 echo "Type coverage:       $TYPE_COVERAGE"
